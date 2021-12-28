e160c95bca17cfb81e5f604ddca65350d34eea90
An Nguyen Hoang <an2419998@gmail.com>
2021-12-28 09:16:57 +0700

    init

---
+++ app/src/main/java/com/annguyenhoang/daggerlearning/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.annguyenhoang.daggerlearning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
